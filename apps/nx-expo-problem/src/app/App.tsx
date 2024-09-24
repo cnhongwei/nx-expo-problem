353ee86e1f329d41076e4cfe6e2d687705dac753
hongwei <cnhongwei@gmail.com>
2024-09-24 17:25:28 +0800

    upgrade nx to 19.8.0

--- apps/nx-expo-problem/src/app/App.tsx
+++ apps/nx-expo-problem/src/app/App.tsx
@@ -139,7 +139,7 @@ export const App = () => {
               <TouchableOpacity
                 style={[styles.listItem, styles.learning]}
                 onPress={() =>
-                  Linking.openURL('https://blog.nrwl.io/?utm_source=nx-project')
+                  Linking.openURL('https://nx.dev/blog/?utm_source=nx-project')
                 }
               >
                 <Svg
@@ -213,7 +213,7 @@ export const App = () => {
                 style={[styles.listItem, styles.learning]}
                 onPress={() =>
                   Linking.openURL(
-                    'https://nx.dev/react-tutorial/1-code-generation?utm_source=nx-project'
+                    'https://nx.dev/nx-api/expo/documents/overview'
                   )
                 }
               >
@@ -252,52 +252,34 @@ export const App = () => {
                   ></Path>
                 </Svg>
               </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.listItem, styles.learning]}
-                onPress={() =>
-                  Linking.openURL(
-                    'https://nxplaybook.com/?utm_source=nx-project'
-                  )
-                }
-              >
-                <Svg
-                  width={24}
-                  height={24}
-                  stroke="#000000"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <Path d="M12 14l9-5-9-5-9 5 9 5z"></Path>
-                  <Path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></Path>
-                  <Path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></Path>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL('https://nx.dev/nx-cloud?utm_source=nx-project')
+              }
+            >
+              <View style={[styles.listItem, styles.shadowBox]}>
+                <Svg width={48} height={48} fill="#000000" viewBox="0 0 24 24">
+                  <Path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
                 </Svg>
                 <View style={styles.listItemTextContainer}>
-                  <Text style={[styles.textMd]}>Video courses</Text>
-                  <Text style={[styles.text2XS, styles.textSubtle]}>
-                    Nx custom courses
+                  <Text
+                    style={[
+                      styles.textMd,
+                      styles.textBold,
+                      styles.marginBottomSm,
+                    ]}
+                  >
+                    Nx is open source
+                  </Text>
+                  <Text style={[styles.textXS, styles.textLight]}>
+                    Love Nx? Give us a star!
                   </Text>
                 </View>
-                <Svg
-                  width={18}
-                  height={18}
-                  stroke="#000000"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <Path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9 5l7 7-7 7"
-                  ></Path>
-                </Svg>
-              </TouchableOpacity>
-            </View>
+              </View>
+            </TouchableOpacity>
           </View>
           <View style={[styles.section, styles.shadowBox]}>
             <TouchableOpacity
@@ -378,17 +360,18 @@ export const App = () => {
           <View style={styles.section}>
             <TouchableOpacity
               onPress={() =>
-                Linking.openURL('https://nx.app/?utm_source=nx-project')
+                Linking.openURL('https://nx.dev/nx-cloud?utm_source=nx-project')
               }
             >
               <View style={styles.shadowBox}>
-                <View style={[styles.listItem, styles.marginBottomMd]}>
+                <View style={[styles.listItem, styles.learning]}>
                   <Svg
                     width={48}
                     height={48}
-                    viewBox="0 0 24 24"
+                    role="img"
                     stroke="currentColor"
                     fill="transparent"
+                    viewBox="0 0 24 24"
                   >
                     <Path
                       strokeWidth="2"
@@ -409,10 +392,16 @@ export const App = () => {
                     >
                       Nx Cloud
                     </Text>
-                    <Text style={[styles.textXS, styles.textLight]}>
+                    <Text style={[styles.textSm, styles.textLight]}>
                       Enable faster CI & better DX
                     </Text>
                   </View>
+                </View>
+                <View style={styles.listItemTextContainer}>
+                  <Text style={[styles.textXS, styles.textLight]}>
+                    You can activate distributed tasks executions and caching by
+                    running:
+                  </Text>
                 </View>
                 <View style={styles.codeBlock}>
                   <Text style={[styles.monospace]}>nx connect</Text>
@@ -420,33 +409,7 @@ export const App = () => {
               </View>
             </TouchableOpacity>
           </View>
-          <View style={styles.section}>
-            <TouchableOpacity
-              onPress={() =>
-                Linking.openURL('https://nx.app/?utm_source=nx-project')
-              }
-            >
-              <View style={[styles.listItem, styles.shadowBox]}>
-                <Svg width={48} height={48} fill="#000000" viewBox="0 0 24 24">
-                  <Path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                </Svg>
-                <View style={styles.listItemTextContainer}>
-                  <Text
-                    style={[
-                      styles.textMd,
-                      styles.textBold,
-                      styles.marginBottomSm,
-                    ]}
-                  >
-                    Nx is open source
-                  </Text>
-                  <Text style={[styles.textXS, styles.textLight]}>
-                    Love Nx? Give us a star!
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </View>
+
           <View
             style={styles.section}
             onLayout={(event) => {
@@ -479,12 +442,14 @@ export const App = () => {
                   />
                 </Svg>
                 <View style={styles.listItemTextContainer}>
-                  <Text style={styles.textSm}>Add UI library</Text>
+                  <Text style={styles.textSm}>
+                    Build, test and lint your app
+                  </Text>
                 </View>
               </View>
-              <View style={[styles.codeBlock, styles.marginBottomLg]}>
+              <View style={styles.codeBlock}>
                 <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # Generate UI lib
+                  # Build
                 </Text>
                 <Text
                   style={[
@@ -493,19 +458,40 @@ export const App = () => {
                     styles.marginBottomMd,
                   ]}
                 >
-                  nx g @nx/expo:lib ui
+                  nx build NxExpoProblem
                 </Text>
                 <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # Add a component
+                  # Test
                 </Text>
-                <Text style={[styles.textXS, styles.monospace]}>nx g \</Text>
-                <Text style={[styles.textXS, styles.monospace]}>
-                  @nx/expo:component \
+                <Text
+                  style={[
+                    styles.textXS,
+                    styles.monospace,
+                    styles.marginBottomMd,
+                  ]}
+                >
+                  nx test NxExpoProblem
+                </Text>
+                <Text style={[styles.textXS, styles.monospace, styles.comment]}>
+                  # Lint
                 </Text>
                 <Text style={[styles.textXS, styles.monospace]}>
-                  ui/src/lib/button
+                  nx lint NxExpoProblem
+                </Text>
+                <Text style={[styles.textXS, styles.monospace, styles.comment]}>
+                  # Run them together!
+                </Text>
+                <Text
+                  style={[
+                    styles.textXS,
+                    styles.monospace,
+                    styles.marginBottomMd,
+                  ]}
+                >
+                  nx run-many -p NxExpoProblem -t build test lint
                 </Text>
               </View>
+
               <View style={styles.listItem}>
                 <Svg
                   width={24}
@@ -527,7 +513,7 @@ export const App = () => {
               </View>
               <View style={[styles.codeBlock, styles.marginBottomLg]}>
                 <Text style={[styles.textXS, styles.monospace]}>
-                  nx show project NxExpoProblem --web
+                  nx show project NxExpoProblem
                 </Text>
               </View>
               <View style={styles.listItem}>
@@ -570,12 +556,12 @@ export const App = () => {
                   />
                 </Svg>
                 <View style={styles.listItemTextContainer}>
-                  <Text style={styles.textSm}>Run affected commands</Text>
+                  <Text style={styles.textSm}>Add UI library</Text>
                 </View>
               </View>
-              <View style={styles.codeBlock}>
+              <View style={[styles.codeBlock, styles.marginBottomLg]}>
                 <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # See what's affected by changes
+                  # Generate UI lib
                 </Text>
                 <Text
                   style={[
@@ -584,28 +570,17 @@ export const App = () => {
                     styles.marginBottomMd,
                   ]}
                 >
-                  nx affected:graph
+                  nx g @nx/react-native:lib ui
                 </Text>
                 <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # run tests for current changes
+                  # Add a component
                 </Text>
-                <Text
-                  style={[
-                    styles.textXS,
-                    styles.monospace,
-                    styles.marginBottomMd,
-                  ]}
-                >
-                  nx affected:text
-                </Text>
-                <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # run e2e tests for current
-                </Text>
-                <Text style={[styles.textXS, styles.monospace, styles.comment]}>
-                  # changes
+                <Text style={[styles.textXS, styles.monospace]}>nx g \</Text>
+                <Text style={[styles.textXS, styles.monospace]}>
+                  @nx/react-native:component \
                 </Text>
                 <Text style={[styles.textXS, styles.monospace]}>
-                  nx affected:e2e
+                  ui/src/lib/button
                 </Text>
               </View>
             </View>
@@ -693,7 +668,7 @@ const styles = StyleSheet.create({
     color: '#6b7280',
   },
   section: {
-    marginVertical: 24,
+    marginVertical: 12,
     marginHorizontal: 12,
   },
   shadowBox: {
